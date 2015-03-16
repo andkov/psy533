906266f8960fc37f6eb781427c59a737b7a55fa4
Andrey Koval <koval.andrey@gmail.com>
2015-03-16 11:17:18 -0700

    ready for class

--- fit.r
+++
@@ -1,6 +0,0 @@
-Factor1: Sentences, Vocabulary, Sent.Completion, 4.Letter.Words, Suffixes, Letter.Series, Pedigrees, Letter.Group, First.Letters
-Factor2: 4.Letter.Words, Suffixes, First.Letters
-Factor3: Sentences, Vocabulary, Sent.Completion
-Letter.Series
-Letter.Group
-
