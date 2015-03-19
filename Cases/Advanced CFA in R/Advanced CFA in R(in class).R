af90a121b9c57f9d3d6bc8710701cee668069791
Andrey Koval <koval.andrey@gmail.com>
2015-03-19 10:23:27 -0700

    IN

---
+++ Cases/Advanced CFA in R/Advanced CFA in R(in class).R
@@ -0,0 +1,82 @@
+rm(list=ls(all=TRUE)) #Clear out variables from previous runs.
+options("scipen"=10, "digits"=5) 
+######### Set Up Working Directory!!
+setwd(dir = "C:/Users/koval_000/Documents/533")
+
+library(Hmisc) # matrix and utility functions
+library(corrgram) # graphing correlation matrices
+library(sem) # structural equation modeling
+
+
+#### Resources ####
+
+## Please consult the website of James Steiger ( www.statpower.net ) for all materials pertaining to this demonstration.
+
+## For the demonstraction of factor analysis with 'sem' package see Steiger's tutorial entitled "CFA with R" ( http://statpower.net/Content/312/Handout/Confirmatory%20Factor%20Analysis%20with%20R.pdf )
+
+## For the demonstration of FA with custom functions see Steiger's tutorial entitled "Advanced CFA with R" ( http://statpower.net/Content/312/Handout/Advanced%20Confirmatory%20Factor%20Analysis%20in%20R.pdf )
+
+## For detailed discussion of 'sem' package see John Fox paper "Structural Equation Modeling in R with the sem Package" ( http://socserv.mcmaster.ca/jfox/Books/Companion/appendix/Appendix-SEMs.pdf )
+
+## For 'sem' package documentation see http://cran.r-project.org/web/packages/sem/sem.pdf or http://www.rdocumentation.org/packages/sem
+
+## For "Exploratory-Confirmatory" Approach see Karl Joreskog 1978 paper ( http://www.researchgate.net/profile/Karl_Joereskog/publication/24062332_Structural_analysis_of_covariance_and_correlation_matrices/links/0046352b8b078d34d6000000.pdf )
+
+#### Custom functions ####
+source("http://statpower.net/Content/312/R%20Stuff/Steiger%20R%20Library%20Functions.txt")
+# Functions to assist you in FA and SEM
+source("http://statpower.net/Content/312/R%20Stuff/AdvancedFactorFunctions.txt")
+# Functions by Dirk Enzmann to aid promax rotation
+source("http://statpower.net/Content/312/R%20Stuff/fa.promax.R")
+
+#### Data and Option prep ####
+
+# Load Athletic data directly from James Stieger's website
+AthleticsData <- read.csv("http://statpower.net/Content/312/Homework/AthleticsData.csv")
+# attach(AthleticsData)
+
+
+# R Correlation/Covariance matrix - R 
+# F F' Grahm-Factors - F
+# VD1/2 D1/2V' Principle Component patterns - 
+# V D V' Earhart-Young decomposition - V, D 
+# Rv = cv Eigenvalues and Eigenvectors
+
+R<-cor(AthleticsData) # correlation matrix R of variables in AthleticsData
+R
+round(cor(AthleticsData),2)
+corrgram(x=R,order=TRUE,
+         main="Correlogram of Athletic Data",
+         upper.panel=panel.pie,
+         lower.panel=panel.shade)
+
+## selection of availible fit/information indices
+# opt <- options(fit.indices = c("GFI", "AGFI", "RMSEA", "NFI", "NNFI","CFI", "RNI", "IFI", "SRMR", "AIC", "AICc", "BIC", "CAIC"))
+opt <- options(fit.indices= c("GFI","RMSEA"))
+
+#### 1. Pure CFA ####
+
+pureCFA.fit.T <- QuickCFA(R=R, n.factors=3, n.obs=1000)
+summary(pureCFA.fit.T)
+GetPattern(pureCFA.fit.T)
+GetPrettyPattern(pureCFA.fit.T)
+RMSEA(pureCFA.fit.T)
+summary(pureCFA.fit.T)[c(1:7)]
+CheckMod(pureCFA.fit.T)
+fit.2 <- UseMod(pureCFA.fit.T)
+
+
+GetPrettyPattern(fit.2)
+
+
+
+
+
+
+
+
+
+
+
+
+
